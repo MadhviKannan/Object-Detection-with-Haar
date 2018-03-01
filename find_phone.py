f4087d74c0db67aaaab5c20a3698fee7064e9d12
Madhvi Kannan <mkannan@usc.edu>
2018-02-28 22:46:43 -0500

    Update find_phone.py

--- find_phone.py
+++ find_phone.py
@@ -1,20 +1,14 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 19 21:25:28 2018
-
-@author: madhvikannan
-"""
+# This code gives the predictions for any test .jpg image based on the cascade.xml file generated. 
 
 import numpy as np
 import cv2
 import sys
 import os
-testDir=sys.argv[1]
+testDir=sys.argv[1] #path to test image
 
 dataDir=os.getcwd()
 
-watch_cascade = cv2.CascadeClassifier(dataDir+'/data/cascade.xml')
+watch_cascade = cv2.CascadeClassifier(dataDir+'/data/cascade.xml') #object to store cascade.xml file
 
 
 img = cv2.imread(testDir)
@@ -25,12 +19,10 @@ height=326
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 watches = watch_cascade.detectMultiScale(gray, 25, 25)
     
-    # add this
 for (x,y,w,h) in watches:
-    #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    unnormalized_x=x+25
+    unnormalized_x=x+25 
     unnormalized_y=y+25
-    x_new=float(unnormalized_x)/float(width)
+    x_new=float(unnormalized_x)/float(width) #getting normalized x,y coordinates
     y_new=float(unnormalized_y)/float(height)
 
 print x_new, y_new
